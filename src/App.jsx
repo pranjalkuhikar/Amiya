d9f42743c47b3f4e2d78fd94ae910c3d255d35f2
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-07-21 21:33:55 +0530

    feat: implement routing and new pages with React Router
    - Added React Router for navigation between pages
    - Created Home, About, and Products pages with corresponding components
    - Integrated new SCSS styling for the Home page
    - Updated App component to include AppRouter for routing management
    - Enhanced package dependencies with react-router-dom and sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import AppRouter from "./routers/AppRouter";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
 };
 
 export default App;
---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Product from "../pages/Products/Products";
+import { Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AppRouter;
