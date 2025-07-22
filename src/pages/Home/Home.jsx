330da8a937eab2ffd5e4b1eeaf5ebf39bd56477d
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-07-22 13:54:41 +0530

    Home page done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "./Home.scss";
+import { MoveRight } from "lucide-react";
 
 const Home = () => {
   return (
     <>
-      <div className="home">Home</div>
+      <div className="home">
+        <div className="home__content">
+          <div className="home__content__title">
+            <span>True</span> to Oneself kind to <span>Nature</span>
+          </div>
+          <div className="home__content__text">
+            Unreservedly honest products that truly work, be kind to skin and
+            the planet - no exceptions!
+          </div>
+        </div>
+        <div className="btn">
+          <div className="line1"></div>
+          <div className="text">Explore All Products</div>
+          <div className="line2">
+            <span>
+              <MoveRight size={20} color="#fff" />
+            </span>
+            <span>
+              <MoveRight size={20} color="#fff" />
+            </span>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
