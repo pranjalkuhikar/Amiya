a505ea5c4b8a42f399725a58069b51b3625003c3
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-07-22 12:48:34 +0530

    Routing setup Done

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
-import Product from "../pages/Products/Products";
+import Shop from "../pages/Shop/Shop";
 import Navbar from "../components/layout/Navbar/Navbar";
-import { Routes, Route } from "react-router-dom";
+import Contact from "../pages/Contact/Contact";
+import Cart from "../pages/Cart/Cart";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 const AppRouter = () => {
   return (
@@ -12,7 +15,12 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/product" element={<Product />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
