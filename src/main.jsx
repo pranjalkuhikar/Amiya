d7b3e165fc3d272fd54fb5a4e379885bd82b06b1
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-07-23 17:06:42 +0530

    Redux Toolkit setup Done

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import LenisProvider from "./components/LenisProvider";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <LenisProvider>
-        <App />
-      </LenisProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <LenisProvider>
+          <App />
+        </LenisProvider>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
